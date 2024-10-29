0940b2aabc9fcfa83b8a8bf6cad0f67a00af299d
Mario D <mariodoka@icloud.com>
2024-10-29 14:30:36 +0100

    moved printBoard func to class Game

--- class_game.cpp
+++ class_game.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
 
 class Game
 {
@@ -8,8 +10,23 @@ public:
     char playerIcons[3] = {'x', 'y', 'z'};
     char gameBoard[8][8];
     const int winCon = 4;
-
+    vector<vector<char>> board;
+    Game() : board(8, vector<char>(8, ' ')) {}
+    void printBoard() {
+        for (int row = 0; row < 8; ++row) {
+            for (int col = 0; col < 8; ++col) {
+                cout << "| " << board[row][col] << " ";
+            }
+            cout << "|" << endl;
+        }
+        
+        for (int col = 0; col < 8; ++col) {
+            cout << "----";
+        }
+        cout << "-" << endl;
+    }
     void runGame()
     {
+        printBoard();
     }
 };
