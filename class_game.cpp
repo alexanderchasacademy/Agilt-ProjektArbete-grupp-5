9a679a0971d841594a0d5f534ba6e09eada7098f
Jessica Sjösten <jessica.e.sjosten@gmail.com>
2024-10-29 13:37:07 +0100

    added code to ask user for their chosen column and a method to handle the placement

--- class_game.cpp
+++ class_game.cpp
@@ -6,10 +6,27 @@ class Game
 {
 public:
     char playerIcons[3] = {'x', 'y', 'z'};
-    char gameBoard[8][8];
+    char gameBoard[8][8] = {{' '}};
     const int winCon = 4;
 
     void runGame()
     {
+        int chosenColumn;
+        int playerIndex = 0;
+        cout << "Where do you want to place your symbol?" << endl;
+        cin >> chosenColumn;
+        placeSymbolInArray(chosenColumn, playerIndex);
+    }
+
+    void placeSymbolInArray(int chosenColumn, int playerIndex)
+    {
+        for (int row = 7; row >= 0; --row)
+        {
+            if (gameBoard[row][chosenColumn] == ' ')
+            {
+                gameBoard[row][chosenColumn] = playerIcons[playerIndex];
+                break;
+            }
+        }
     }
 };
