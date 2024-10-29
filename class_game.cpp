e8b2c76ece21705dfe52f71d89915f023768476b
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2024-10-29 12:05:57 +0100

    added a method to class that allows the player to set their own symbols.

--- class_game.cpp
+++ class_game.cpp
@@ -5,7 +5,24 @@ using namespace std;
 class Game
 {
     public:
+    //initial player icons
     char playerIcons[3] = {'x', 'y', 'z'};
+    void setPlayerIcons()
+    {
+        int symbolChoice = 0;
+        cout << "tryck 1 om du vill bestämma dina egna symboler, annars tilldelas spelarna standard symboler(x, y, z)\n";
+        cin >> symbolChoice;
+
+        if (symbolChoice == 1)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                cout << "Välj symbol för spelare " << i + 1 << ": ";
+                cin >> playerIcons[i]; 
+            }
+        }
+    }
+    //array containing the gameboard
     char gameBoard[8][8];
     const int winCon = 4;
 };
