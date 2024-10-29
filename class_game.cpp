23923da1f89650bfa310db0c84cfc778a3263039
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2024-10-29 13:47:32 +0100

    added a method to switch player

--- class_game.cpp
+++ class_game.cpp
@@ -5,9 +5,28 @@ using namespace std;
 class Game
 {
 public:
+    char currentPlayer = NULL;
     char playerIcons[3] = {'x', 'y', 'z'};
     char gameBoard[8][8];
     const int winCon = 4;
+    
+    //method för att byta aktiva spelare
+    void switchPlayer()
+    {
+        if (currentPlayer == playerIcons[0])
+        {
+            currentPlayer == playerIcons[1];
+        }
+        else if (currentPlayer == playerIcons[1])
+        {
+            currentPlayer == playerIcons[2];
+        }
+        else if (currentPlayer == playerIcons[2])
+        {
+            currentPlayer == playerIcons[0];
+        }
+        else;
+    }
 
     void runGame()
     {
