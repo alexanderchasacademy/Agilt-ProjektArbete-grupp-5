acb96d0346869223de157cfac1979aed79c6b1cd
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2024-10-29 11:04:01 +0100

    myfirstcommit

---
+++ Connect_Four.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "header.h"
+#include "class_game.cpp"
+
+using namespace std;
+
+int main()
+{
+    Game connectFour;   
+    return 0; 
+}
---
+++ class_game.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+class Game
+{
+    public:
+    char playerIcons[3] = {'x', 'y', 'z'};
+    char gameBoard[8][8];
+    const int winCon = 4;
+};
